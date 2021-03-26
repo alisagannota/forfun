4ca3f7704a178b918c6bc945482e23c0cc176707
alisagannota <77847538+alisagannota@users.noreply.github.com>
2021-03-26 16:32:12 +0300

    update layout

--- script.js
+++ script.js
@@ -10,19 +10,4 @@ function duplicate() {
     original.parentNode.appendChild(clone);
 }
 
-let container = document.querySelector ('#container');
 
-
-for (let i= 0; i < 20; i++) {
-    let circle = document.createElement ('div');
-    circle.classList.add ('circle');
-    circle.style.left = (90 * Math.random ()) + '%';
-    circle.style.top = (90 * Math.random ()) + '%';
-    container.appendChild (circle);
-}
-
-container.addEventListener('click', function (event) {
-    if (event.target.classList.contains ('circle')) {
-        event.target.remove();
-    }
-})
